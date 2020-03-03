03355031beebc32286ecca37a955b3a561b04699
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-03 22:40:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,10 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   def initialize(name, artist, genre)
-    @@name = name
-    @@artist = artist 
-    @@genre = genre 
-    @@count
+    @name = name
+    @artist = artist 
+    @genre = genre 
+    @count
   end
   
   def self.count 
